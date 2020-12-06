1462c5036607a66f8b64cd97ecb78db887360948
Archi00 <albert.archi@gmail.com>
2020-12-06 16:35:37 +0100

    Created gitignore folder

--- dist/mikro-orm.config.js
+++ dist/mikro-orm.config.js
@@ -1,15 +1,12 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
 Object.defineProperty(exports, "__esModule", { value: true });
 const constants_1 = require("./constants");
 const Post_1 = require("./entities/Post");
-const assword_1 = __importDefault(require("p../assword"));
+const password_1 = require("../password");
 exports.default = {
     entities: [Post_1.Post],
     dbName: 'lireddit',
-    password: assword_1.default,
+    password: password_1.password,
     type: 'postgresql',
     debug: !constants_1.__prod__,
 };
